80115ee6aadb4484aa9eedfa300a7a586342c76e
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-01-03 23:53:18 +0530

    Create fact_check.py

---
+++ fact_check.py
@@ -0,0 +1,19 @@
+import requests
+
+def fact_check(query):
+    api_url = "https://factchecktools.googleapis.com/v1alpha1/claims:search"
+    params = {
+        "query": query,
+        "key": "your_google_api_key"
+    }
+    
+    response = requests.get(api_url, params=params)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+# Example usage
+fact_check_results = fact_check("COVID-19 vaccine safety")
+print(fact_check_results)
