58d65bd2374a08646142209b1a0772ee9916f9b1
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-01-03 23:50:14 +0530

    Create data_collection_facebook.py

---
+++ data_collection_facebook.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+ACCESS_TOKEN = 'your_facebook_access_token'
+FACEBOOK_PAGE_ID = 'your_facebook_page_id'
+
+def collect_facebook_data(query, max_results=50):
+    url = f"https://graph.facebook.com/{FACEBOOK_PAGE_ID}/posts?fields=id,message,created_time&access_token={ACCESS_TOKEN}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        posts = response.json()['data']
+        post_data = []
+        for post in posts:
+            if query.lower() in post.get('message', '').lower():
+                post_data.append({
+                    'id': post['id'],
+                    'message': post.get('message', ''),
+                    'created_time': post.get('created_time', '')
+                })
+        return post_data
+    else:
+        return None
+
+# Example usage
+facebook_posts = collect_facebook_data("misinformation", 50)
+print(json.dumps(facebook_posts, indent=2))
