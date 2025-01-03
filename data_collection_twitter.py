b7a563f96b1968dcf0768d41da4361fd6c5d9fed
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2025-01-04 01:14:09 +0530

    Update data_collection_twitter.py

--- data_collection_twitter.py
+++ data_collection_twitter.py
@@ -1,17 +1,17 @@
 import tweepy
 import json
 
-# Twitter API credentials
+
 consumer_key = 'your_consumer_key'
 consumer_secret = 'your_consumer_secret'
 access_token = 'your_access_token'
 access_token_secret = 'your_access_token_secret'
 
-# Authenticate to the Twitter API
+
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Collect tweets by a specific hashtag or keyword
+
 def collect_tweets(keyword, count=100):
     tweets = tweepy.Cursor(api.search_tweets, q=keyword, lang="en").items(count)
     tweet_data = []
@@ -27,6 +27,6 @@ def collect_tweets(keyword, count=100):
     
     return tweet_data
 
-# Example usage
+
 tweets = collect_tweets("misinformation", 50)
 print(json.dumps(tweets, indent=2))
